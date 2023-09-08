a469f1b03e842da9a775b464b4a2f657bd546da4
icha2811 <143861206+icha2811@users.noreply.github.com>
2023-09-08 21:19:36 +0700

    Add files via upload

---
+++ balok.py
@@ -0,0 +1,12 @@
+# Menghitung volume balok
+
+panjang = int(input("Masukkan panjang :"))
+lebar =  int(input("Masukkan lebar :"))
+tinggi = int(input("Masukkan tinggi :"))
+p = panjang
+l = lebar
+t = tinggi
+volume = p*l*t
+
+print("Volume balok adalah =", volume)
+
