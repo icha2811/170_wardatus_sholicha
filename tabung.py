ae3f043098f0069b440d7de1d8fc995f355a3018
icha2811 <143861206+icha2811@users.noreply.github.com>
2023-09-08 21:02:42 +0700

    Add files via upload

---
+++ tabung.py
@@ -0,0 +1,10 @@
+# Menghitung volume tabung
+
+tinggi = int(input("Masukkan tinggi tabung :"))
+alas = int(input("Masukkan jari-jari tabung :"))
+t = tinggi 
+a = alas
+p = 3.14     #nilai dari pi
+volume = p*a**2*t
+
+print("volume tabung adalah..." , volume)
